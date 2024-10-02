fd1b017147b4d2d3beae6f9a5261b4b4c5b1db15
Tamanna Afrin <afrintamanna926@gmail.com>
2024-10-02 20:39:14 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,22 @@
+  #include<iostream>
+  using namespace std;
+
+  class Fruit{
+
+    public:
+    string name;
+    string color;
+    };
+
+    int main(){
+        Fruit apple; //object
+        apple.name ="Apple";
+        apple.color ="Red";
+        cout<<apple.name<<" _ "<<apple.color<<endl;
+
+        Fruit * mango = new Fruit();
+        mango->name = "Mango";
+        mango->color ="Yellow";
+        cout<<mango->name<<" _ "<<mango->color<<endl;
+        return 0;
+    }
